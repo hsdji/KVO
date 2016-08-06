334c28ff09ca91e4c0e3c0549f53221032dcfed1
单小飞 <2530101715@qq.com>
2016-08-06 21:35:53 +0800

    Initial Commit

---
+++ KVO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVO
+//
+//  Created by 小飞 on 16/8/6.
+//  Copyright © 2016年 小飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KVO/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KVO
+//
+//  Created by 小飞 on 16/8/6.
+//  Copyright © 2016年 小飞. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
